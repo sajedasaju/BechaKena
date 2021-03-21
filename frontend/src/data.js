d68d05cd6ab3dc5e82f6fc9056a2cbbf880b943e
Sajeda Akhter <sajedasaju85@gmail.com>
2021-03-22 00:23:40 +0600

    Added Code

---
+++ frontend/src/data.js
@@ -0,0 +1,47 @@
+export default {
+    products:[{
+        _id:"1",
+        name: "Jamdani Sharee",
+        category:"Sharee",
+        image:"/images/d4.jpg",
+        price:330,
+        brand:"Oloshoi",
+        rating: 4.5,
+        numReviews: 10
+
+    },
+    {
+        _id:"2",
+        name: "Jamdani Sharee",
+        category:"Sharee",
+        image:"/images/d3.jpg",
+        price:330,
+        brand:"Oloshoi",
+        rating: 4.5,
+        numReviews: 10
+
+    },
+    {
+        _id:"3",
+        name: "T-Shirt",
+        category:"Shirt",
+        image:"/images/d2.jpg",
+        price:40,
+        brand:"Oloshoi",
+        rating: 4.5,
+        numReviews: 10
+
+    },
+    {
+        _id:"4",
+        name: "Brow Pomade",
+        category:"Makeup",
+        image:"/images/d1.jpg",
+        price:30,
+        brand:"Oloshoi",
+        rating: 4.5,
+        numReviews: 10
+
+    },
+]
+}
